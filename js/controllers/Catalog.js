ae3a4a4185f2c3c0464127a20e0b510d80c92642
PustotaApp <86097389+PustotaApp@users.noreply.github.com>
2021-06-18 13:57:21 +0700

    Add files via upload

---
+++ js/controllers/Catalog.js
@@ -0,0 +1,81 @@
+var AllList =
+{
+  telescop:
+  [
+    {
+      index: 0,
+      name: "Монитор AOC 24B1H",
+      price: 263,
+      size: "108.0×37.0×23.0",
+      imgURL: "img/moni/BK1149EQ1.png"
+    },
+    {
+      index: 1,
+      name: "Монитор AOC 24G2U5/BK",
+      price: 777,
+      mount: "AZ-GTe SynScan GOTO",
+      size: "85.0×43.5×26.0",
+      imgURL: "img/moni/MAK102.png"
+    },
+    {
+      index: 2,
+      name: "Монитор AOC I2481FXH",
+      price: 255,
+      manufacturer: "Bresser ",
+      type: "Reflector",
+      dmm: 76, //Диаметр главного зеркала
+      focus: 700,
+      mount: "Azimuthal",
+      size: "83.0×31.0×26.0",
+      imgURL: "img/moni/BresserVenus.png"
+    },
+    {
+      index: 3,
+      name: "Монитор AOC C27G2U/BK",
+      price: 944,
+      manufacturer: "Sky-Watcher",
+      type: "Catadioptric",
+      dmm: 127, //Диаметр главного зеркала
+      focus: 1500,
+      mount: "Equatorial mount EQ3-2",
+      size: "101.0×28.0×45.5",
+      imgURL: "img/moni/MAK127.png"
+    },
+    {
+      index: 4,
+      name: "Монитор AOC C32G2AE/BK",
+      price: 1067,
+      manufacturer: "Sky-Watcher",
+      type: "Reflector",
+      dmm: 200, //Диаметр главного зеркала
+      focus: 1000,
+      mount: "Equatorial mount EQ5",
+      size: "106.0×104.0×30.0",
+      imgURL: "img/moni/P2001.png"
+    },
+    {
+      index: 5,
+      name: "Монитор AOC G2590PX",
+      price: 420,
+      manufacturer: "Sky-Watcher",
+      type: "Reflector",
+      dmm: 114, //Диаметр главного зеркала
+      focus: 500,
+      mount: "Dobson Automated Mount",
+      size: "59.0×35.5×34.0",
+      imgURL: "img/moni/dob114500.png"
+    },
+    {
+      index: 6,
+      name: "Монитор AOC CQ32G2SE/BK",
+      price: 3555,
+      manufacturer: "Sky-Watcher",
+      type: "Refractor",
+      dmm: 150, //Диаметр главного зеркала
+      focus: 1200,
+      mount: "Equatorial mount EQ6",
+      size: "166.0×54.0×31.0",
+      imgURL: "img/moni/BK15012.png"
+    }
+  ]
+}
